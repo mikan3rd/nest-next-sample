247887a2cf24401607ca38eebb5c47d539a466b8
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-10-22 21:48:13 +0900

    refactor: nestjsのディレクトリ整理

---
+++ packages/backend/src/modules/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+
+import { AppController } from "../controllers/app.controller";
+import { AppService } from "../services/app.service";
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- packages/backend/test/app.e2e-spec.ts
+++ packages/backend/test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import * as request from "supertest";
 
-import { AppModule } from "./../src/app.module";
+import { AppModule } from "../src/modules/app.module";
 
 describe("AppController (e2e)", () => {
   let app: INestApplication;
@@ -17,9 +17,6 @@ describe("AppController (e2e)", () => {
   });
 
   it("/ (GET)", () => {
-    return request(app.getHttpServer())
-      .get("/")
-      .expect(200)
-      .expect("Hello World!");
+    return request(app.getHttpServer()).get("/").expect(200).expect("Hello World!");
   });
 });
